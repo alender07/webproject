65ba8a7326407d22010ba4bdd20e2149298adf7b
alender07 <alender07@yandex.ru>
2017-11-19 22:49:03 +0400

    add project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase); //добавляем инфу о покупке в БД
+            db.SaveChanges(); //сохраняем в БД все изменения
+            return "Спасибо за покупку, "+ purchase.Person + "!";
+        }
+
+        [HttpGet] //ERROR HERE
+        public ActionResult Delet(int id)
+        {
+            //if (id!=null)
+            ViewBag.BookId = id;
+            return View();
+        }
+        
+        [HttpPost]
+        public string Delet(Purchase purchase ) //удаление из БД
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Remove(purchase);
+            db.SaveChanges();
+            return "Книга удалена!";
+        }
+            
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class BookContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; } //типизировано моделью book
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookObInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Бойцовский клуб", Author = "Чак Паланик", Price = 400});
+            db.Books.Add(new Book { Name = "451 градус по Фаренгейту", Author = "Рэй Брэдбери", Price = 380});
+            db.Books.Add(new Book { Name = "Зов Ктулху", Author = "Говард Лавкрафт", Price = 350});
+           
+            base.Seed(db);
+        }
+    }
+}
